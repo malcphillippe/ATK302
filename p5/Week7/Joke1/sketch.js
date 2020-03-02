00ee3eef9cf4e0bcaa56f4e8dba93558c74b48d4
malcphillippe <59883708+malcphillippe@users.noreply.github.com>
2020-03-01 23:06:39 -0600

    updated changes

--- p5/Week7/Joke1/sketch.js
+++ p5/Week7/Joke1/sketch.js
@@ -1,7 +1,39 @@
+var myState = 0;
+
 function setup() {
   // put setup code here
+  createCanvas(800, 800);
 }
 
 function draw() {
   // put drawing code here
+  switch (myState) {
+    case 0:
+      background('Blue')
+      text("Problem Child");
+      break;
+
+    case 1:
+      background('yellow');
+      fill('black')
+      text("Gods Child Satians Angel");
+      break;
+
+
+    }
+
+
+function mouseReleased() {
+  myState = myState +1;
+  if (myState > 1) {
+    myState = 0
+  }
+
+
+
+function mouseReleased() {
+  myState = myState + 1;
+  if (myState > 1 ) {
+    myState = 0;
+  }
 }
--- p5/Week7/Joke2/sketch.js
+++ p5/Week7/Joke2/sketch.js
@@ -1,7 +1,53 @@
+var myState = 0;
+var timer = 0;
+
 function setup() {
   // put setup code here
+  createCanvas(800, 800);
 }
 
 function draw() {
   // put drawing code here
+  switch (myState) {
+    case 0:
+      background('Blue')
+      text("Hey I Invented A New Word", 100, 100);
+
+timer ++ ;
+
+      timer = timer + 1;
+      if (timer > 200) {
+        myState = 1;
+        timer = 0
+
+      }
+      break;
+
+    case 1:
+      background('yellow');
+      fill('black')
+      text("Plagiarism", 100, 100);
+      break;
+
+timer ++;
+
+    timer = timer + 1;
+    if (timer > 200) {
+      myState = 0;
+      timer = 0
+
+    }
+      break;
+
+
+  }
+
+}
+
+
+function mouseReleased() {
+  myState = myState + 1;
+  if (myState > 1) {
+    myState = 0;
+  }
 }
