9b383b2de0f7a00c1a066e9299bb987fc9f58333
malcphillippe <59883708+malcphillippe@users.noreply.github.com>
2020-04-12 23:22:53 -0500

    game finished

--- p5/week12/oop/sketch.js
+++ p5/week12/oop/sketch.js
@@ -1,26 +1,70 @@
 var maxcars = 20;
 var cars = [];
 var frogPos;
-var myState = 0;
-var maxtime = 12*60;
-var timer = 0 ;
+var myState = -1;
+var maxtime = 12 * 60;
+var timer = 0;
+var spongebob
+var scary
+var party
+var song1;
+var song2;
+var song3;
+var song4;
+var x = 0;
+var shark;
+var fish;
+function preload() {
+  song1 = loadSound('assets/bK.mp3');
+  song2 = loadSound('assets/gotstar.mp3');
+  song3 = loadSound('assets/playerdead.mp3');
+  song4 = loadSound('assets/level-win.mp3');
+
+}
 
 function setup() {
   createCanvas(800, 800);
-
+  textAlign(CENTER);
+  imageMode(CENTER);
+  spongebob = loadFont('assets/spongeBob.ttf');
+  scary = loadFont('assets/scary.ttf');
+  party = loadFont('assets/party.TTF');
+  welcome = loadImage('assets/welcome.png');
+shark = loadImage('assets/shark.png');
+fish = loadImage('assets/fish.png');
   for (var i = 0; i < maxcars; i++) {
     cars.push(new Car());
   }
 
   frogPos = createVector(400, height - 100);
-
+  //song1.loop();
+  //song1.stop();
+  //song2.loop();
+  // song2.stop();
+  // song3.loop();
+  // song3.stop();
+  // song4.loop();
+  // song4.stop();
 }
 
 function draw() {
   switch (myState) {
+    case -1:
+      song2.loop();
+      myState = 0;
+      break;
     case 0:
       background('Blue')
-      text("Howdy Folks", 100, 100);
+      image(welcome, width/2, height/2, width, height);
+      textFont(spongebob, 120);
+      textSize(60);
+      fill('225, 228, 71, 100');
+      text("Click To Play", x, 650);
+      x++;
+      if (x > width) {
+        x = 0;
+      }
+
       break;
 
     case 1:
@@ -28,34 +72,50 @@ function draw() {
       timer++;
       if (timer > maxtime) {
         myState = 3;
+        song1.pause();
+        song4.loop();
       }
       break;
 
     case 2: //win state
+
       background('yellow');
-      fill('green')
-      text("Congratulations", 100, 100);
+      textFont(party, 120);
+      textSize(140);
+      fill('0, 203, 255, 100');
+      text("Congratulations", width/2, height/2);
       break;
 
     case 3:
       background('yellow');
-      fill('green')
-      text("Try Again", 100, 100);
+      fill('black')
+      textFont(scary, 120);
+      textSize(140);
+      text("Try Again", width/2, height/2);
+      x++;
+      if (x > width) {
+        x = 0;
+      }
       break;
 
 
 
   }
 }
+
 function reset() {
   timer = 0;
+    song3.pause();
+    song4.pause();
+    song2.play();
   cars = [];
   for (var i = 0; i < maxcars; i++) {
     cars.push(new Car());
   }
-  frogPos.x = width/2;
-  frogPos.y = height-100;
+  frogPos.x = width / 2;
+  frogPos.y = height - 100;
 }
+
 function game() {
   background('lightblue');
 
@@ -66,12 +126,16 @@ function game() {
       cars.splice(i, 1);
     }
   }
-if (cars.length == 0) {
-  myState = 2;
-}
+  if (cars.length == 0) {
+  song1.stop();
+    song3.loop();
+    myState = 2;
+
+  }
   // frog
-  fill('green');
-  ellipse(frogPos.x, frogPos.y, 50, 50);
+  //fill('green');
+  //ellipse(frogPos.x, frogPos.y, 50, 50);
+image(shark,frogPos.x, frogPos.y, 150,170);
   checkForKeys();
 
 }
@@ -79,6 +143,8 @@ if (cars.length == 0) {
 function mouseReleased() {
   switch (myState) {
     case 0:
+    song2.pause();
+    song1.loop();
       myState = 1;
       break;
     case 2:
@@ -113,8 +179,9 @@ function Car() {
   this.b = random(255);
   // methods
   this.display = function() {
-    fill(this.r, this.g, this.b);
-    rect(this.pos.x, this.pos.y, 30, 30);
+    //fill(this.r, this.g, this.b);
+    //rect(this.pos.x, this.pos.y, 30, 30);
+    image(fish,this.pos.x, this.pos.y, 50, 50 );
   }
 
   this.drive = function() {
